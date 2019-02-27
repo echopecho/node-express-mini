3d25c35e9764d9fe1e8cfd18d1f6a2ca791323c1
echopecho <pbsmithtbn@gmail.com>
2019-02-27 10:22:58 -0600

    Add get and post API calls
    I set up the basic API to the database. Right now only the get and post requests work. Next I will set up the ability to get a specific user by id.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // implement your API here
+const express = require('express');
+
+const db = require('./data/db.js');
+
+const server = express();
+const port = '8000';
+
+server.use(express.json());
+
+server.get('/api/users', (req, res) => {
+  db.find()
+    .then(users => {
+      res.send(users);
+    })
+    .catch(() => {
+      res.status(500).json({error: 'The users information could not be retrieved.'});
+    })
+});
+
+server.post('/api/users', (req, res) => {
+  const newUser = req.body;
+
+  if(newUser.name && newUser.bio) {
+    db.insert(newUser)
+      .then(() => {
+        res.status(201).json(newUser);
+      })
+      .catch (() => {
+        res.status(500).json({error: "There was an error while saving the user to the database."})
+      })
+  } else {
+    res.status(400).json({error: "Please provide name and bio for the user."})
+  }
+})
+
+
+server.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
+})
