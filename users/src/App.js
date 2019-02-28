033e2559b89c6d0bd586f474dd65a3463f3dd983
echopecho <pbsmithtbn@gmail.com>
2019-02-27 20:47:21 -0600

    Add form to create new user
    I completed the function needed to add a new user to the database.

--- users/src/App.js
+++ users/src/App.js
@@ -7,7 +7,9 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      users: []
+      users: [],
+      nameText: "",
+      bioText: ""
     }
   }
 
@@ -15,6 +17,26 @@ class App extends Component {
     axios.get('http://localhost:8000/api/users')
       .then(res => {
         this.setState({ users: res.data })
+      })
+  }
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  addUser = e => {
+    e.preventDefault();
+    let newUser = {
+      name: this.state.nameText,
+      bio: this.state.bioText
+    }
+    let newState = this.state.users;
+
+    axios.post('http://localhost:8000/api/users', newUser)
+      .then(response => {
+        console.log(response.data)
+        newState.push(response.data);
+        this.setState({ users: newState });
       })
   }
 
@@ -29,6 +51,25 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <form onSubmit={this.addUser}>
+          <input 
+            type="text"
+            name="nameText"
+            value={this.state.nameText}
+            onChange={this.handleChange}
+            placeholder="Enter name"
+          >
+          </input>
+          <input 
+            type="text"
+            name="bioText"
+            value={this.state.bioText}
+            onChange={this.handleChange}
+            placeholder="Enter bio"
+          >
+          </input>
+          <button type="submit">Add user</button>
+        </form>
         {this.state.users.map(user => (
           <div key={user.id}>
             <p>{user.name}</p>
