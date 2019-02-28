6e86ffdacae07c417ab821efb20446018761ac8a
echopecho <pbsmithtbn@gmail.com>
2019-02-27 18:22:50 -0600

    Add delete and put API calls
    I completed the final two CRUD methods for this project. The put request was tricky because what the database returns when updating a user.

--- index.js
+++ index.js
@@ -18,6 +18,22 @@ server.get('/api/users', (req, res) => {
     })
 });
 
+server.get('/api/users/:id', (req, res) => {
+  const { id } = req.params;
+
+  db.findById(id)
+    .then(user => {
+      if(user) {
+        res.send(user);
+      } else {
+        res.status(404).json({error: "The user with the specified ID does not exist."});
+      }
+    })
+    .catch(() => {
+      res.status(500).json({error: 'The users information could not be retrieved.'});
+    })
+})
+
 server.post('/api/users', (req, res) => {
   const newUser = req.body;
 
@@ -27,12 +43,75 @@ server.post('/api/users', (req, res) => {
         res.status(201).json(newUser);
       })
       .catch (() => {
-        res.status(500).json({error: "There was an error while saving the user to the database."})
+        res.status(500).json({error: "There was an error while saving the user to the database."});
       })
   } else {
-    res.status(400).json({error: "Please provide name and bio for the user."})
+    res.status(400).json({error: "Please provide name and bio for the user."});
   }
+});
+
+server.delete('/api/users/:id', (req, res) => {
+  const { id } = req.params;
+
+  db.findById(id)
+    .then(user => {
+      if(user) {
+        db.remove(id)
+          .then(() => {
+            res.status(201).json({success: "User was deleted."});
+          })
+          .catch(() => {
+            res.status(500).json({error: "The user could not be removed."});
+          })
+      } else {
+        res.status(404).json({error: "The user with the specified ID does not exist."});
+      }
+    })
+});
+
+server.put('/api/users/:id', (req, res) => {
+  const { id } = req.params;
+  const updateUser = req.body;
+  console.log(updateUser);
+
+  // db.update(id, updateUser)
+  //   .then(test => {
+  //     if(test === 1) {
+  //       db.findById(id)
+  //         .then(updated => {
+  //           res.send(updated)
+  //         })
+  //     }
+  //   })
+  //   .catch(err => {
+  //     res.status(500).json({error: "Why?!"});
+  //   })
+
+  db.findById(id)
+    .then(user => {
+      if(user) {
+        if(updateUser.name && updateUser.bio) {
+        db.update(id, updateUser)
+          .then(test => {
+            if(test === 1) {
+              db.findById(id)
+                .then(updated => {
+                  res.status(200).json(updated)
+                })
+            }
+          })
+          .catch(() => {
+            res.status(500).json({error: "The user information could not be modified."});
+          })
+        } else {
+          res.status(400).json({errorMessage: "Please provide name and bio for the user."})
+        }
+      } else {
+        res.status(404).json({error: "The user with the specified ID does not exist."})
+      }
+  })
 })
+
 
 
 server.listen(port, () => {
